efac7f00d0325cf479b8acece5cf0aa250785bc8
derva1 <vdervisevi1@etf.unsa.ba>
2022-12-18 15:33:47 +0100

    Code is  optimized for parallelization. Some helper funtions have been added.

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <chrono>
-#include <algorithm>
-#include <cstring>
 #include <cstdlib>
+#include <random>
+#include <chrono>
 #include <omp.h>
 
-using namespace std;
+const int matrix_size = 1000;
+const int num_threads = 4;
 
 class Timer {
     typedef std::chrono::high_resolution_clock clock_;
@@ -16,7 +16,7 @@ public:
     Timer(const char* header = "") : beg_(clock_::now()), header(header) {}
     ~Timer() {
         double e = elapsed();
-        cout << header << ": " << e / 1000000 << " seconds" << endl;
+        std::cout << header << ": " << e / 1000000 << " seconds" << std::endl;
     }
     void reset() {
         beg_ = clock_::now();
@@ -25,6 +25,62 @@ public:
         return std::chrono::duration_cast<second_>(clock_::now() - beg_).count();
     }
 };
+
+// Helper functions that are used for cleaning code
+
+double** generateMatrixA() {
+    double** A = (double**)malloc(sizeof(double*) * matrix_size);
+    for (int i = 0; i < matrix_size; i++) {
+        A[i] = (double*)malloc(sizeof(double) * matrix_size);
+    }
+
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_real_distribution<double> distribution(1, 1000);
+
+
+    for (int i = 0; i < matrix_size; i++) {
+        for (int j = 0; j < matrix_size; j++) {
+            A[i][j] = distribution(generator);
+        }
+    }
+
+    return A;
+}
+
+double** generateMatrixLU() {
+    double** A = (double**)malloc(sizeof(double*) * matrix_size);
+    for (int i = 0; i < matrix_size; i++) {
+        A[i] = (double*)malloc(sizeof(double) * matrix_size);
+    }
+
+    for (int i = 0; i < matrix_size; i++) {
+        for (int j = 0; j < matrix_size; j++) {
+            A[i][j] = 0;
+        }
+    }
+
+    return A;
+}
+
+void deallocateMemory(double** A) {
+    for (int i = 0; i < matrix_size; i++) {
+        free(A[i]);
+    }
+    free(A);
+}
+
+void printMatrix(double** A) {
+    std::cout << "Matrix elements are: " << std::endl;
+    for (int i = 0; i < matrix_size; i++) {
+        for (int j = 0; j < matrix_size - 1; j++) {
+            std::cout << A[i][j] << ", ";
+        }
+        std::cout << A[i][matrix_size - 1] << std::endl;
+    }
+}
+
+// Algorithms for LU decomposition, different variations
 
 void crout_0(double** A, double** L, double** U, int n) {
     int i, j, k;
@@ -56,11 +112,15 @@ void crout_0(double** A, double** L, double** U, int n) {
 void crout_1(double** A, double** L, double** U, int n) {
     int i, j, k;
     double sum = 0;
+#pragma omp parallel for
     for (i = 0; i < n; i++) {
         U[i][i] = 1;
     }
+#pragma omp parallel for private(i,j,k,sum) schedule(dynamic, 8)
     for (j = 0; j < n; j++) {
-#pragma omp parallel for private(i,k,sum)
+        if (j == n - 1)
+            std::cout << "Number of threads being used: " << omp_get_num_threads() << std::endl;
+        //#pragma omp parallel for private(i,k,sum)
         for (i = j; i < n; i++) {
             sum = 0;
             for (k = 0; k < j; k++) {
@@ -68,7 +128,7 @@ void crout_1(double** A, double** L, double** U, int n) {
             }
             L[i][j] = A[i][j] - sum;
         }
-#pragma omp parallel for private(i,k,sum)
+        //#pragma omp parallel for private(i,k,sum)
         for (i = j; i < n; i++) {
             sum = 0;
             for (k = 0; k < j; k++) {
@@ -80,211 +140,62 @@ void crout_1(double** A, double** L, double** U, int n) {
             U[j][i] = (A[j][i] - sum) / L[j][j];
         }
     }
-}
-
-void crout_2(double** A, double** L, double** U, int n) {
-    int i, j, k;
-    double sum = 0;
-    for (i = 0; i < n; i++) {
-        U[i][i] = 1;
-    }
-    for (j = 0; j < n; j++) {
-        //i=j case
-        sum = 0;
-        for (k = 0; k < j; k++) {
-            sum = sum + L[j][k] * U[k][j];
-        }
-        L[j][j] = A[j][j] - sum;
-        int x = j + (n - j) / 2;
-#pragma omp parallel sections
-        {
-#pragma omp section
-            {
-                for (int i = j + 1; i < x; i++) {
-                    double sum = 0;
-                    for (int k = 0; k < j; k++) {
-                        sum = sum + L[i][k] * U[k][j];
-                    }
-                    L[i][j] = A[i][j] - sum;
-                }
-            }
-#pragma omp section
-            {
-                for (int i = x; i < n; i++) {
-                    double sum = 0;
-                    for (int k = 0; k < j; k++) {
-                        sum = sum + L[i][k] * U[k][j];
-                    }
-                    L[i][j] = A[i][j] - sum;
-                }
-
-            }
-#pragma omp section
-            {
-                for (int i = j; i < x; i++) {
-                    double sum = 0;
-                    for (int k = 0; k < j; k++) {
-                        sum = sum + L[j][k] * U[k][i];
-                    }
-                    if (L[j][j] == 0) {
-                        exit(0);
-                    }
-                    U[j][i] = (A[j][i] - sum) / L[j][j];
-                }
-            }
-#pragma omp section
-            {
-                for (int i = x; i < n; i++) {
-                    double sum = 0;
-                    for (int k = 0; k < j; k++) {
-                        sum = sum + L[j][k] * U[k][i];
-                    }
-                    if (L[j][j] == 0) {
-                        exit(0);
-                    }
-                    U[j][i] = (A[j][i] - sum) / L[j][j];
-                }
-            }
-        }
-    }
 
 }
 
-void testCroutSequencial(double** A, size_t n) {
-    double** L = (double**)malloc(sizeof(double*) * n);
-    for (size_t i = 0; i < n; i++) {
-        L[i] = (double*)malloc(sizeof(double) * n);
-    }
 
-    double** U = (double**)malloc(sizeof(double*) * n);
-    for (size_t i = 0; i < n; i++) {
-        U[i] = (double*)malloc(sizeof(double) * n);
-    }
+// Testing methods
 
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            L[i][j] = 0;
-            U[i][j] = 0;
-        }
-    }
-    
+void testCroutSequencial(double** A, int n) {
+    double** L = generateMatrixLU();
+    double** U = generateMatrixLU();
+
     {
-        Timer t("SEQUENTIAL: ");
+        Timer t("SEQUENTIAL");
         crout_0(A, L, U, n);
     }
 
-    for (size_t i = 0; i < n; i++) {
-        free(L[i]);
-        free(U[i]);
-    }
-
-    free(L);
-    free(U);
+    deallocateMemory(L);
+    deallocateMemory(U);
 }
 
-void testCroutParallel1(double** A, size_t n) {
-    double** L = (double**)malloc(sizeof(double*) * n);
-    for (size_t i = 0; i < n; i++) {
-        L[i] = (double*)malloc(sizeof(double) * n);
-    }
-
-    double** U = (double**)malloc(sizeof(double*) * n);
-    for (size_t i = 0; i < n; i++) {
-        U[i] = (double*)malloc(sizeof(double) * n);
-    }
-
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            L[i][j] = 0;
-            U[i][j] = 0;
-        }
-    }
+void testCroutParallel1(double** A, int n) {
+    double** L = generateMatrixLU();
+    double** U = generateMatrixLU();
 
     {
-        Timer t("PARALLEL 1: ");
+        Timer t("PARALLEL 1");
         crout_1(A, L, U, n);
     }
 
-    for (size_t i = 0; i < n; i++) {
-        free(L[i]);
-        free(U[i]);
-    }
-
-    free(L);
-    free(U);
+    deallocateMemory(L);
+    deallocateMemory(U);
 }
 
-void testCroutParallel2(double** A, size_t n) {
-    double** L = (double**)malloc(sizeof(double*) * n);
-    for (size_t i = 0; i < n; i++) {
-        L[i] = (double*)malloc(sizeof(double) * n);
-    }
 
-    double** U = (double**)malloc(sizeof(double*) * n);
-    for (size_t i = 0; i < n; i++) {
-        U[i] = (double*)malloc(sizeof(double) * n);
-    }
 
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            L[i][j] = 0;
-            U[i][j] = 0;
-        }
-    }
+int main()
+{
+    double** A = generateMatrixA();
 
-    {
-        Timer t("PARALLEL 2: ");
-        crout_2(A, L, U, n);
-    }
+    testCroutSequencial(A, matrix_size);
 
-    for (size_t i = 0; i < n; i++) {
-        free(L[i]);
-        free(U[i]);
-    }
-
-    free(L);
-    free(U);
-}
-
-int main() {
-    cout << "Unesite velicinu niza: ";
-    int n;
-    cin >> n;
-    cout << endl;
-    double** A = (double**)malloc(sizeof(double*) * n);
-    for (size_t i = 0; i < n; i++) {
-        A[i] = (double*)malloc(sizeof(double) * n);
-    }
-
-    srand(time(0));
-
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            A[i][j] = rand() % 100;
-        }
-    }
-
-    //testCroutSequencial(A,n);
-
-    /*cout << endl << "Unesite broj threadova: ";
-    int thr;
-    cin >> thr;*/
-
-    omp_lock_t lock;
-
-    omp_init_lock(&lock);
+    std::cout << std::endl;
 
     omp_set_dynamic(0);
-    omp_set_nested(1);
-    omp_set_num_threads(4);
-   
-    testCroutParallel1(A, n);
-    testCroutParallel2(A, n);
+    omp_set_num_threads(num_threads);
+    std::cout << "Maximum number of threads on current device: " << omp_get_max_threads() << std::endl;
+    std::cout << "Matrix size: " << matrix_size << "x" << matrix_size << std::endl;
 
-    for (size_t i = 0; i < n; i++) {
-        free(A[i]);
+    for (int i = 0; i < 10; i++) {
+
+        std::cout << "TRY " << i + 1 << ":" << std::endl;
+        testCroutParallel1(A, matrix_size);
+        //testCroutParallel2(A, matrix_size);
+
+        std::cout << std::endl;
     }
-    free(A);
-
+    deallocateMemory(A);
     return 0;
+
 }
